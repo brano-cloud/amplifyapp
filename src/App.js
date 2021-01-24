e82af8392f828ab08766e511bb4df475e43a6f24
brano-cloud <branocico@hotmail.com>
2021-01-24 15:56:42 +0000

    added ccp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import CCP from './ccp';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload1.
+          Edit <code>src/App.js</code> and save to reload2.
         </p>
         <a
           className="App-link"
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <CCP />
     </div>
   );
 }
---
+++ src/ccp.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import React from 'react';
+import 'amazon-connect-streams';
+
+class CCP extends Component {
+    constructor(props) {
+        super(props);
+        this.containerDiv = React.createRef();
+    }
+
+    componentDidMount() {
+        // eslint-disable-next-line no-undef
+        connect.core.initCCP(this.containerDiv.current, {
+            ccpUrl: "https://brano.awsapps.com/connect/ccp-v2",
+            loginPopup: true,
+            loginPopupAutoClose: true,
+            region: 'us-east-1',
+            softphone: {
+                allowFramedSoftphone: true
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="ccp">
+                {/* ccp */}
+                <div className="containerDiv" ref={this.containerDiv} />
+            </div>
+        );
+    }
+}
+
+export default CCP;
