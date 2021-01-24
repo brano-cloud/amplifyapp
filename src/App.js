58769db1268a5e88f320aeb285fc2aed37fca34a
brano-cloud <branocico@hotmail.com>
2021-01-24 19:16:15 +0000

    styling changed

--- src/App.js
+++ src/App.js
@@ -5,22 +5,10 @@ import './App.css';
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload2.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <CCP />
+    <div className="flex-container">
+      <CCP className="flex-item-left" />
+      <div className="flex-item-right">
+      </div>
     </div>
   );
 }
