b3b075f3eb3fef78b1a68d4089a157df4b6277da
brano-cloud <branocico@hotmail.com>
2021-01-26 17:22:37 +0000

    css change

--- src/ccp.js
+++ src/ccp.js
@@ -39,11 +39,11 @@ class CCP extends Component {
             contact.onEnded(function () {
                 console.log(`onEnded`);
             });
-            contact.onConnected(() => {
+            contact.onConnected((ddd) => {
                 console.log(`onConnected(${contact.getContactId()})`);
                 let attributeMap = contact.getAttributes();
                 console.log(JSON.stringify(attributeMap));
-                console.log('xxx', this.props);
+                console.log('xxx', this.props, ddd);
                 try {
                     this.props.handleOpenUrl(`https://${attributeMap.testAttr.value}`)
                     //window.open(`https://${attributeMap.testAttr.value}`);
